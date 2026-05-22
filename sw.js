7268c1cd818981440a9b9f70685423c240ceada5
bhaskarsaikia503-source <bhaskarsaikia503@gmail.com>
2026-05-22 14:39:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-self.addEventListener('install', () => {
+const CACHE_NAME = "grocery-tracker-v1";
+
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', event => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
